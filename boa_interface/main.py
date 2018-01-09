a53ed8dc2f0115d17379edfc8f1367b48ae2b025
EmreKurt <officialemre@gmail.com>
2018-01-08 21:14:42 -0500

    change email format slightly

--- boa_interface/main.py
+++ boa_interface/main.py
@@ -15,17 +15,16 @@ def main():
     candle_ltc = candlesticks.CandleSticks("LTC-USD")
 
     coin_names = [
-        #"ripple", "ethereum", "monero","stellar","nav-coin","nem","dash",
-        "bitcoin","litecoin","ethereum","bitcoin-cash"
+        "ripple", "ethereum", "monero", "stellar", "nav-coin", "nem", "dash",
+        "bitcoin", "litecoin"
     ]
 
     coins = []
     for coin in coin_names:
         coins.append(crypto_object.CryptoObject(coin,
-                                      scrape_obj.get_price_history(coin), 4))
+                                                scrape_obj.get_price_history(coin), 4))
 
-
-    OFFICIAL_BOA(coins,now)
+    OFFICIAL_BOA(coins, now)
     # text_message_report(quick_report(coins,now))
     print(quick_report(coins, now))
 
@@ -94,16 +93,17 @@ def OFFICIAL_BOA(coins, now):
     import smtplib
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
-    s = smtplib.SMTP(host='smtp.privateemail.com', port=587)
-    s.starttls()
+    s = smtplib.SMTP_SSL(host='smtp.privateemail.com', port=465)
     s.login("admin@cryptoboa.io", "Aesthetics21!P")
 
     me = "admin@cryptoboa.io"
-    you = ["officialemre@gmail.com", "cryptoboa@exode.com", "lechalex1@gmail.com",
-           "bhomsi@gmail.com", "bskb04@gmail.com", "redguitarfreak88@gmail.com",
-           "zotthewizard@gmail.com", "dnhvcrpt@gmail.com", "tartrate@gmail.com", "nicastrh@gmail.com",
-            "jonathanng222@gmail.com", "alexswenews@gmail.com", "dvddvdsn777@gmail.com", "akoruth95@gmail.com",
-           "miroslavstricevic@gmail.com","sixohofficial@gmail.com"]
+    you = ["officialemre@gmail.com"]
+    # you = ["officialemre@gmail.com", "cryptoboa@exode.com", "lechalex1@gmail.com",
+    #        "bhomsi@gmail.com", "bskb04@gmail.com", "redguitarfreak88@gmail.com",
+    #        "zotthewizard@gmail.com", "dnhvcrpt@gmail.com", "tartrate@gmail.com", "nicastrh@gmail.com",
+    #         "jonathanng222@gmail.com", "alexswenews@gmail.com", "dvddvdsn777@gmail.com", "akoruth95@gmail.com",
+    #        "miroslavstricevic@gmail.com","kninjas@gmail.com","biggt620@gmail.com","davidbeddow92@gmail.com",
+    #        "sixohofficial@gmail.com"]
 
     for name in you:
         # Create message container - the correct MIME type is multipart/alternative.
@@ -116,7 +116,8 @@ def OFFICIAL_BOA(coins, now):
         html = '''<html>
                         <head>
                             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-                            <style>body{ margin:0 100; background:whitesmoke; }</style>
+                            <!-- Optional theme -->
+                            <link rel="stylesheet" href="cryptoboa.io/css/creative.css">
                             <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
                         </head>
                         <body>
@@ -127,50 +128,33 @@ def OFFICIAL_BOA(coins, now):
             tldr_val = coin.get_prediction_plus(
                 coin.get_most_recent_pattern()) if coin.get_stationarity() else coin.get_stationarity_as_string()
             html += '''
-                    
-                            <br/>
                             <h2> ''' + coin.get_name() + '''</h2>
   
-                                    <h3>
+                                <h4>
                                     <b> TL;DR: ''' + tldr_val + '''</b>
-                                    </h3>
+                                </h4>
+                                <ul>
+                                    <li><b>''' + coin.get_stationarity_as_string() + '''</b>, with a Chi-Square sigma of ''' + coin.get_stat_sigma() + '''
+                                    and a Chi-Square critical value of ''' + coin.get_stat_critical_value() + '''</li>
+                                    <li><b>''' + coin.get_randomness_as_string() + '''ly</b> distributed, with a Chi-Square sigma of 
+                                        ''' + coin.get_rand_sigma() + ''' and a Chi-Square critical value of ''' + coin.get_rand_critical_value() + '''
+                                    <li><b>''' + coin.get_independence_as_string() + '''</b>, with a Chi-Square sigma of ''' + coin.get_ind_sigma() + '''
+                                    and a Chi-Square critical value of ''' + coin.get_ind_critical_value() + '''</li>
+                                </ul>
 
-                            <p> Here's the rundown on <b>''' + coin.get_name() + '''</b> today:
-                                <br/>
-                                <br/>
-                                    Over the last <b>''' + coin.get_time_period() + ''' ''' + coin.get_name() + '''</b> price change 
-                                    has been <b>''' + coin.get_stationarity_as_string() + '''</b>, with a Chi-Square sigma of <b>''' + coin.get_stat_sigma() + '''</b> 
-                                    and a Chi-Square critical value of <b>''' + coin.get_stat_critical_value() + '''</b> . 
-                                    That means that <b>''' + coin.get_name() + ''' ''' + coin.get_likelihood(
-                coin.get_stationarity()) + '''</b> 
-                                    to keep acting the way it has been acting. 
-                                <br/>
-                                <br/>
-                                    Price change has been <b>''' + coin.get_randomness_as_string() + '''</b>ly distributed, with a Chi-Square sigma of 
-                                    <b>''' + coin.get_rand_sigma() + '''</b> and a Chi-Square critical value of <b>''' + coin.get_rand_critical_value() + '''</b>. That means that 
-                                    <b>''' + coin.get_name() + '''</b> price  <b>''' + coin.get_likelihood(
-                coin.get_randomness()) + '''</b> selected from the
-                                    probability distribution. 
-                                <br/>
-                            <p> 
-                                Over the last <b>''' + coin.get_time_period() + ''' ''' + coin.get_name() + '''</b> price change 
-                                has been <b>''' + coin.get_independence_as_string() + '''</b>, with a Chi-Square sigma of <b>''' + coin.get_ind_sigma() + '''</b> 
-                                and a Chi-Square critical value of <b>''' + coin.get_ind_critical_value() + '''</b>. That means that for
-                                    <b>''' + coin.get_name() + '''</b> it <b>''' + coin.get_likelihood(
-                not coin.get_independence()) + '''</b> that we can predict
-                                    whether the price will go up or down tomorrow.                    
+                                      
 
-                                After analysing the price history, BOA concluded that there is a <b>''' + '%.2f' % coin.get_odds_of_increase(
-                coin.get_most_recent_pattern()) + '''%</b>
+                                <p>After analysing the price history, BOA concluded that there is a <b>''' + '%.2f' % coin.get_odds_of_increase(
+                                coin.get_most_recent_pattern()) + '''%</b>
                                 chance price will go up, and a <b>''' + '%.2f' % coin.get_odds_of_decrease(
-                coin.get_most_recent_pattern()) + '''%</b>
+                                coin.get_most_recent_pattern()) + '''%</b>
                                 chance price will go down. The average magnitude of the up move is <b>''' + '%.2f' % coin.get_magnitude_of_increase(
-                coin.get_most_recent_pattern()) + '''%</b>
+                                coin.get_most_recent_pattern()) + '''%</b>
                                 and the average magnitude of the down move is <b>''' + '%.2f' % coin.get_magnitude_of_decrease(
-                coin.get_most_recent_pattern()) + '''%</b>.
+                                coin.get_most_recent_pattern()) + '''%</b>.
                                 Today, you should <b>''' + coin.get_prediction_plus(coin.get_most_recent_pattern()) + '''</b>.
+                                </p>
                                 <br/>
-                                <br/>   
                 '''
 
         html += " <p> See you tomorrow!  <br/><br/> Disclaimer: The information provided here and in accompanying material " \
